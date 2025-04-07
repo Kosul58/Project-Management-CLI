e851ffb277bd618b3d48121b728b4bf31c6f9696
Kosul58 <kosulgrg@gmail.com>
2025-04-07 19:09:26 +0545

    tserror

---
+++ Backend/server.ts
@@ -0,0 +1,30 @@
+type preson = {
+  name: String;
+  age: Number;
+};
+
+interface student {
+  name: String;
+  age: Number;
+  address?: String;
+  phone?: String;
+}
+
+const person1: preson = {
+  name: "ram",
+  age: 22,
+};
+
+// console.log(person1);
+
+let x;
+const addStudent = (args: preson): boolean => {
+  const student: student = args;
+  student.address = "Pokhara";
+  student.phone = "45454215542124";
+  x = student;
+  return student.age ? true : false;
+};
+
+console.log(addStudent({ name: "kk", age: 55 }));
+console.log(x);
---
+++ dist/server.js
@@ -0,0 +1,16 @@
+// "use strict";
+// const person1 = {
+//     name: "ram",
+//     age: 22,
+// };
+// // console.log(person1);
+// let x;
+// const addStudent = (args) => {
+//     const student = args;
+//     student.address = "Pokhara";
+//     student.phone = "45454215542124";
+//     x = student;
+//     return student.age ? true : false;
+// };
+// console.log(addStudent({ name: "kk", age: 55 }));
+// console.log(x);
